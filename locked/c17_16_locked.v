a480857cb009f9dac6270dd55c2b9f4ba22f3810
aldokoestiwa <aldokoestiwa07@gmail.com>
2026-02-04 09:34:05 +0700

    mengganti rangkaian c17

--- locked/c17_16_locked.v
+++ locked/c17_16_locked.v
@@ -9,8 +9,8 @@ module c17_locked(
     input clk, rst_n,
     input [15:0] static_key,
     input [15:0] correct_TK,
-    input output_key_bit;
-    output O1,O2,
+    input output_key_bit,
+    output O1,O2
   );
 
   wire W1,W2,W3,W3_locked,W4,O1_locked;
@@ -27,7 +27,7 @@ module c17_locked(
   assign  W1 = ~(I1 & I3),
           W2 = ~(I3 & I4),
           W3 = ~(I2 & W2),
-          W3_locked = W3 ^ (~set);
+          W3_locked = W3 ^ (~set),
           W4 = ~(W2 & I5),
           O1_locked = ~(W1 & W3_locked),
           O1 = ~(O1_locked ^ output_key_bit),
@@ -76,10 +76,10 @@ module nonlinear_gen_16bit (
 
   genvar i;
   generate
-    for (i=0;i<4;i++) begin : nonlinear_key_16bit_inst
+    for (i = 0; i < 4; i = i + 1) begin : nonlinear_key_16bit_inst
       nonlinear_gen nonlinear_key_16bit (
-        .K(K_16[(i*4)+3 : i*4])     // K_16[3:0], K_16[7:4], K_16[11:8], K_16[15:12]
-        .TK(TK_16[(i*4)+3 : i*4])   // TK_16[3:0], TK_16[7:4], TK_16[11:8], TK_16[15:12]
+        .K(K_16[(i*4)+3 : i*4]),     // K_16[3:0], K_16[7:4], K_16[11:8], K_16[15:12]
+        .TK(TK_16[(i*4)+3 : i*4])    // TK_16[3:0], TK_16[7:4], TK_16[11:8], TK_16[15:12]
       );
     end
   endgenerate
@@ -94,5 +94,7 @@ module nonlinear_gen_16bit (
       end
       else begin
         count <= 4'b0000;
-    end  
+      end    
+    end
+  end
 endmodule
